a5ab81fc90ad4c19147fa7c45001f40c40e1bc41
jerry earnest <getoffmeubz@gmail.com>
2018-11-30 09:38:03 -0700

    new work

---
+++ if statements olympics/index.js
@@ -0,0 +1,25 @@
+if(5 > 3) {
+    console.log("is greater than");
+} if (cat = 3) {
+    console.log("is the lenth of");
+} if ("cat === dog"); {
+    console.log("not the same")
+}
+var person = {
+    name: "bobby",
+    age: 12
+}
+
+
+if(person.name[0] === "b" && person.age >= 18){
+    console.log("let into movies")
+} else  console.log("bobby cant go to the movies")
+
+if (1 === "1"){
+    console.log("strict")
+    else if( 1 == "1") {
+        console.log ("loose")
+    }
+    if(1 <= 2 && 2 === 4){
+        console.log("yes")
+    }
