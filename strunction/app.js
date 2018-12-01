aca7cb398684ef67b6b4ebf51e18309e49665d25
jerry earnest <getoffmeubz@gmail.com>
2018-12-01 15:13:44 -0700

    complete color grid from scratch

---
+++ strunction/app.js
@@ -0,0 +1,49 @@
+var str1 = "hey";
+var str2 = "what are you doing";
+var str3 = "eating";
+
+
+
+var finalString = str1.concat(str2, str3);
+
+console.log(finalString);
+
+var str = "what are you doing today miss";
+var i = str.indexOf("miss");
+console.log(i);
+
+
+var str = "hopefully i can pass"
+var n = str.lastIndexOf("pass");
+console.log(n);
+
+
+
+var str = "what type of vidoe games can i make";
+var p = str.match("/doe/q");
+console.log(p);
+
+var str = "baking soda";
+var q = str.replace("baking", "rootbeer");
+console.log(q)
+
+
+var  str = "jerry earnest";
+var d = str.slice(1 , 5);
+console.log(d);
+
+
+var str = "dragonball z";
+var i = str.toLowerCase();
+console.log(i)
+
+
+var str = "jerry wayne";
+var i = str.toUpperCase();
+console.log(i)
+
+
+
+var str = "almight is good";
+var i = str.substring(1, 5);
+console.log(i)
