aebb16ee3a057645016262b5bc720757b7ab5af0
jerry earnest <getoffmeubz@gmail.com>
2018-12-03 18:19:07 -0700

    css flaggs and array methods started and completed

---
+++ arraymethodse/app.js
@@ -0,0 +1,21 @@
+var fruit = ["banana", "apple", "orange", "watermelon"];
+var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
+console.log(vegetables.pop());
+
+var fruit = ["banana", "apple", "orange", "watermelon"];
+var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
+console.log(fruit.shift("banana"));
+
+
+console.log(fruit.indexOf("orange"));
+
+fruit.push(18);
+console.log(fruit);
+
+console.log(vegetables.length);
+
+vegetables.push(4);
+console.log(vegetables);
+
+var food = fruit.concat(vegetables);
+console.log(food);
